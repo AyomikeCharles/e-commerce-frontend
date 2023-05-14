cb9a63dcaecf7c7b5e6c033cc3f8db41b8072996
AyomikeCharles <charlesayomike4@gmail.com>
2023-05-14 18:11:04 +0100

    more design

--- src/components/admin/utils/AHeader.tsx
+++ src/components/admin/utils/AHeader.tsx
@@ -1,37 +1,64 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faBell, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBell, faSearch } from '@fortawesome/free-solid-svg-icons';
 import MobileAside from './MobileAside';
 import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../utils/images/logo.png'
+
+
 const AHeader = () =>{
 
-    const [isOpen, setIsOpen] = useState(false);
+    const [Open, setOpen] = useState(false);
+    const navigate = useNavigate()
+    const [search, setSearch] = useState('')
 
 
     const handleAsideState = () => {
-        setIsOpen(!isOpen)
+        setOpen(!Open)
     } 
+
+   
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        const {value} = e.target
+        setSearch(value)
+    }
+
+    const handleSearch = () => {
+        navigate(`/search/${search}`)
+    }
 
     return(
         <>
             <nav className="w-full bmd:flex justify-between px-10 py-5 bg-slate-800 text-white">
-                <div className='mb-3 bmd:mb-0'>
+                <div className='block bmd:hidden mb-3 bmd:mb-0'>
                     <div className='flex justify-between bmd:hidden'>
-                        <span><Link to='/'><img className='w-24 h-16' loading='lazy' src={logo} alt="logo"/></Link></span>
+                        <div className='w-full flex'>
+                            <span className='pt-1 border px-3 rounded' onClick={handleAsideState}><FontAwesomeIcon icon={faBars} size='lg'  /></span>
+                            <span><Link to='/'><img className='w-10/12' loading='lazy' src={logo} alt="logo"/></Link></span>
 
-                        <span onClick={handleAsideState}><FontAwesomeIcon icon={faBars} size='lg'  /></span>
+                        </div>
+                        
+                        <ul className="flex justify-center bmd:justify-end">
+                            <li className='relative mx-5 pt-1'><span className='text-sm hover:text-lime-500 transition duration-500'><FontAwesomeIcon icon={faBell} size='lg' className='text-lime-500' /></span><span className='bg-white absolute -top-1 -right-[2px] w-4 h-4 text-black text-center text-[9px] rounded-[50%]'>0</span></li>
+                        </ul>
                     </div>
                 </div>
-                <div>
-                    <ul className="flex justify-center bmd:justify-end">
-                        <li className='pt-2 relative mx-3'><span className='text-sm hover:text-lime-500 transition duration-500'><FontAwesomeIcon icon={faBell} size='lg'  /></span><span className='bg-white absolute -top-0 -right-[2px] w-4 h-4 text-black text-center text-[9px] rounded-[50%]'>0</span></li>
-                        <li className='pt-2 mx-3'><span className='text-sm hover:text-lime-500 transition duration-500'><FontAwesomeIcon icon={faMoon} size='lg'/></span></li>
-                    </ul>
+                <div className='md:w-6/12'>
+                    <form>
+                        <input type="text" value={search} onChange={handleChange} className='w-5/6 text-black px-2 focus:outline-none rounded-l h-10'/>
+                        <button onClick={handleSearch} className='w-1/6 bg-lime-500 rounded-r h-10 text-white'><FontAwesomeIcon icon={faSearch}/></button>
+                    </form>
                 </div>
+                    <ul className="hidden bmd:flex bmd:justify-end mt-2">
+                        <li className='relative mx-5'><span className='text-sm hover:text-lime-500 transition duration-500'><FontAwesomeIcon icon={faBell} size='lg' className='text-lime-500' /></span><span className='bg-white absolute -top-2 -right-[2px] w-4 h-4 text-black text-center text-[9px] rounded-[50%]'>0</span></li>
+                       
+                    </ul>
+                
             </nav>
+
             <aside className='bmd:hidden'>
-                <MobileAside open={isOpen} changeOpenState ={handleAsideState}/>
+                <MobileAside open={Open} changeOpenState ={handleAsideState}/>
             </aside>
         </>
     )
--- src/components/admin/utils/MobileAside.tsx
+++ src/components/admin/utils/MobileAside.tsx
@@ -1,10 +1,11 @@
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBagShopping, faDashboard, faDoorOpen, faList, faList12, faListAlt, faLocation, faReceipt, faShoppingBag, faTimes, faUser, faUserCheck, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useRef } from 'react';
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import TokenService from "../../../res/tokenService"
 import { useAppDispatch } from "../../.."
 import { reset, logout } from "../../../slicer/authSlice"
+import logo from '../../utils/images/logo.png'
 
 type Props = {
     open:boolean,
@@ -53,58 +54,68 @@ const MobileAside:React.FC<Props> = ({open, changeOpenState}):JSX.Element =>{
     return(
         <>
             
-                {/* <section  className="top-0 -right-[100%] fixed min-h-full w-9/12 md:w-2/6 xl:w-4/12 z-20 bg-gray-100 drop-shadow-lg"> */}
-                <nav ref={cartDrawer} id="sidebar" className="fixed -left-[100%] overflow-y-scroll transition duration-500 top-0 pt-5 w-full h-full z-50 bg-slate-300">
+
+                <nav ref={cartDrawer} id="sidebar" className="fixed -left-[100%] overflow-y-scroll transition duration-500 top-0 pt-5 w-full h-full z-50 bg-white">
 
                     <div className="flex justify-between border-b px-10 py-3">
-                        <div className=""><Link to='/'>Logo</Link></div>
+                        <div className=""><Link to='/'><img className='w-10/12' loading='lazy' src={logo} alt="logo"/></Link></div>
 
                         <div className='flex justify-end'>
 
-                            <button onClick={changeOpenState} className="mx-5">
-                                <FontAwesomeIcon icon={faTimes} size='2xl' className='drop-shadow-sm'/>
-                            </button>
+                        <button onClick={changeOpenState} className="mx-5 border px-3 rounded">
+                            <FontAwesomeIcon icon={faTimes} size='xl' className='drop-shadow-sm font-light'/>
+                        </button>
 
                         </div>
                     </div>            
     
-                <div className="flex justify-center py-10">
+                <div className="flex text-lg py-10">
             
                     <ul>
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/dashboard'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faDashboard} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin'>Dashboard</Link>
                         </li>
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/products'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faListAlt} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/products'>Products</Link>
                         </li>
     
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/categories'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faList12} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/categories'>Categories</Link>
                         </li>
                         
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/orders'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faBagShopping} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/orders'>Orders</Link>
                         </li>
 
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/invoice'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faReceipt} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/> 
                             <Link to='/admin/invoice'>Invoice</Link>
                         </li>
             
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/users'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faUsers} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/users'>Users</Link>
                         </li>
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/admins'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faUserCheck} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/admins'>Admins</Link>
                         </li>
 
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/region'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faLocation} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/region'>Region</Link>
                         </li> 
 
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 ${location.pathname === '/admin/transactions'?'bg-lime-500/50':null} py-3`}>
+                            <FontAwesomeIcon icon={faUser} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <Link to='/admin/profile'>Profile</Link>
                         </li>
                         <li onClick={changeOpenState} className={`my-1 rounded  px-8 py-3`}>
+                            <FontAwesomeIcon icon={faDoorOpen} size='lg' className='drop-shadow-sm mr-3 text-lime-500'/>
                             <button onClick={handleLogOut}>Logout</button>
                         </li>
                     </ul>
